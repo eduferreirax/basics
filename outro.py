a6edf7926da4a58639ef8bac70b976f97a2fec32
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-01-05 14:17:24 -0300

    Add files via upload

--- outro.py
+++ outro.py
@@ -1,3 +1,5 @@
+#REVISAO DIA 05/01/2026
+
 # Read a value of floating point with two decimal places. This represents  
 # a monetary value. 
 # After this, 
@@ -31,20 +33,51 @@
 # 0 moeda(s) de R$ 0.05
 # 3 moeda(s) de R$ 0.01
 
+# N = float(input())
+# valor = int(round(N * 100))
+
+# Notas = [10000,5000,2000,1000,500,200]
+# Moedas = [100,50,25,10,5,1]
+
+# print("NOTAS:")
+# for nota in Notas:
+#     qtd = valor // nota
+#     valor = valor % nota
+#     print(f"{qtd} nota(s) de R$ {nota/100:.2f}")
+# print("MOEDAS:")
+# for moeda in Moedas:
+#     qtd1 = valor // moeda
+#     valor = valor % moeda
+#     print(f"{qtd} moeda(s) de R$ {moeda/100:.2f}")
+
+
+# A, B, C, D = map(int, input())
+
+# if (B > C) and (D > A) and ((C + D) > (A + B)) and (C > 0) and (D > 0) and (A % 2 == 0):
+#     print("Valores aceitos")
+# else:
+#     print("Valores nao aceitos")
+
+
+    #A, B, C, D = map(int, input().split())
+#A, B, C, D = [int(input()) for _ in range(4)]
+
 N = float(input())
-print(N)
 
-Notas = [100,50,20,10,5,2]
-Moedas = [1,0.50,0.25,0.10,0.05,0.01]
+valor = int(round(N * 100))
 
-resto = N 
+NOTAS = [10000,5000,2000,1000,500,200]
 
-for nota in Notas:
-    qtd = resto // nota
-    resto = resto % nota
-for moeda in Moedas:
-    qtd1 = resto // moeda
-    resto = resto % moeda
-print(f"NOTAS:{qtd}nota(s) de R${nota:2f}")
-print(f"MOEDAS:{qtd1}moeda(s) de R${moeda:2f}")
+MOEDAS = [100,50,25,10,5,1]
 
+print("NOTAS:")
+for nota in NOTAS:
+    qtd = valor // nota
+    valor = valor % nota
+    print(f"{qtd} nota(s) de R$ {nota/100:.2f}")
+
+print("MOEDAS:")
+for moeda in MOEDAS:
+    qtd = valor // moeda
+    valor = valor % moeda
+    print(f"{qtd} moeda(s) de R$ {moeda/100:.2f}")
