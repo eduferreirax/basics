94c7deb1a7d06e4011bdb118604b6cb1f4c95135
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-01-31 00:46:12 -0300

    Add files via upload

--- stilltrying.py
+++ stilltrying.py
@@ -33,3 +33,58 @@ if x != 0 and y == 0:
     print("Eixo X")
 if x == 0 and y != 0:
     print("Eixo Y")
+
+#1043
+
+a, b, c = map(float, input().split())
+
+soma1 = b + c
+soma2 = a + b
+soma3 = a + c
+
+if(a < soma1 and b < soma3 and c < soma2):
+
+    perimetro = a + b + c
+
+    print("Perimetro = {0:.1f}".format(perimetro))
+
+else:
+
+    area = ((a + b) * c) / 2
+
+    print("Area = {0:.1f}".format(area))
+
+#1044 
+
+A, B = map(int, input().split())
+
+if (A % B == 0) or (B % A == 0):
+    print("Sao Multiplos")
+    
+else:
+    print("Nao sao Multiplos")
+
+#1045
+
+A, B, C = map(float, input().split())
+a, b, c = sorted([A, B, C], reverse=True)
+
+# 1) Primeiro: verifica se forma triângulo
+if a >= b + c:
+    print("NAO FORMA TRIANGULO")
+else:
+    # 2) Tipo pelo ângulo (usa quadrados)
+    a2, b2, c2 = a*a, b*b, c*c
+
+    if a2 == b2 + c2:
+        print("TRIANGULO RETANGULO")
+    elif a2 > b2 + c2:
+        print("TRIANGULO OBTUSANGULO")
+    else:
+        print("TRIANGULO ACUTANGULO")
+
+    # 3) Tipo pelos lados
+    if a == b == c:
+        print("TRIANGULO EQUILATERO")
+    elif a == b or b == c or a == c:
+        print("TRIANGULO ISOSCELES")
