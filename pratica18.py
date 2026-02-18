658d76a3cd5e8fbd15d6cda7c3f3fb7d530a328b
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-18 04:43:56 -0300

    added #1078

--- pratica18.py
+++ pratica18.py
@@ -4,3 +4,20 @@ n = int(input())
 
 for i in range(2, n+1, 2):
     print(f"{i}^2 = {i*i}")
+
+#1074
+
+n = int(input())
+
+for i in range(n):
+    x = int(input())
+    if x == 0:
+        print("NULL")
+    if x % 2 == 0 and x > 0:
+        print("EVEN POSITIVE")
+    elif x % 2 == 0 and x < 0:
+        print("EVEN NEGATIVE")
+    if x % 2 != 0 and x > 0:
+        print("ODD POSITIVE")
+    elif x % 2!= 0 and x < 0:
+        print("ODD NEGATIVE")
