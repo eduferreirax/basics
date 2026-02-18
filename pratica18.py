15ebd2aefb1af4d549bdee1068e0757c52d301b5
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-18 04:55:44 -0300

    added #1078 tree version

--- pratica18.py
+++ pratica18.py
@@ -11,13 +11,18 @@ n = int(input())
 
 for i in range(n):
     x = int(input())
+    
     if x == 0:
         print("NULL")
-    if x % 2 == 0 and x > 0:
-        print("EVEN POSITIVE")
-    elif x % 2 == 0 and x < 0:
-        print("EVEN NEGATIVE")
-    if x % 2 != 0 and x > 0:
-        print("ODD POSITIVE")
-    elif x % 2!= 0 and x < 0:
-        print("ODD NEGATIVE")
+    
+    elif x % 2 == 0:
+        if x < 0:
+            print("EVEN NEGATIVE")
+        else:
+            print("EVEN POSITIVE")
+        
+    else:
+        if x > 0:
+            print("ODD POSITIVE")
+        else:
+            print("ODD NEGATIVE")
